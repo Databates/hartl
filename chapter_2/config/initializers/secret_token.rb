81a89669226ad12699766075c66caed51a93ee3c
Databates <38bates@gmail.com>
2014-05-10 10:39:58 -0500

    generated skeleton and ran users migration

---
+++ chapter_2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chapter2::Application.config.secret_key_base = '0f11eba2ee9b15bf70f028b763b7e7510974cc70bf585bfafea8a63d6c1deabc5539a6ea26ed5c53430dbf9ffd2dfab175da4c765f90bf7bd43cd0c6f388685b'
