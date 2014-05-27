e520b2d9036b329b1ccd0839442a46ae8fe43cbc
Databates <38bates@gmail.com>
2014-05-27 11:19:52 -0700

    Completed Chapter 2 of Hartl Tutorial

--- chapter_2/app/models/micropost.rb
+++ chapter_2/app/models/micropost.rb
@@ -1,5 +1,14 @@
 class Micropost < ActiveRecord::Base
+	belongs_to :user
 	validates :content, length: { maximum: 140 }
 
 
 end
+
+
+# This has_many and belongs_to relationship b/t User and Micropost respectively
+# allows for the following: 
+# Here we have accessed the user’s microposts using the code first_user.microposts: 
+# with this code, Active Record automatically returns all the microposts with user_id 
+# equal to the id of first_user (in this case, 1). We’ll learn much more about the 
+# association facilities in Active Record in Chapter 10 and Chapter 11.
