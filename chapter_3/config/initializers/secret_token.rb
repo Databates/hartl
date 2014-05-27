7ba2132dbc07a1bdd9318247b3435efac370c15f
Databates <38bates@gmail.com>
2014-05-27 11:30:48 -0700

    Created Chapter 3 project

---
+++ chapter_3/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chapter3::Application.config.secret_key_base = 'f246a32225c88e47e7b26359d6ba0788950017dbff26ab64e11d27403d42b4bc49f14edafe3015d2ac6cf5a23218c66d2b4e575516598a461017432dba957fbf'
